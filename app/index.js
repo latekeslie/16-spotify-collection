b1271d4909c83b6c4c571b939b66224fdee4906e
Kate Leslie <thisizk8@gmail.com>
2017-03-01 20:56:49 -0600

    did some html, and I think most of the js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
 import 'whatwg-fetch';
+import Vue from 'vue';
+import App from './app.vue';
+
+//Start app component w/ vue
+new Vue({
+  ...App,
+  el: '.app',
+});
---
+++ app/song-item.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <li class="track">
+    <img src="" alt="" class="track__frame">
+    <h3 class="track__artist"> {{ track.artist }} </h3>
+    <p class="track__name"> {{ track.name }} </p>
+
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['track'],
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
